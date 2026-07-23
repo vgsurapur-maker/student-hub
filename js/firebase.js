89080626e2d6fa3936293a6e398ced53fa38f12f
vgsurapur-maker <vgsurapur@gmail.com>
2026-07-23 05:40:20 +0530

    update website

--- js/firebase.js
+++ js/firebase.js
@@ -19,3 +19,5 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
+import { getAnalytics } from "firebase/analytics";
+const analytics = getAnalytics(app);
